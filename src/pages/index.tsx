b10f4c621219b8c75e144885041f0ddbfdf1d2ee
rodolfo wolff <rodolfoluizwolff@gmail.com>
2023-01-13 09:43:40 -0300

    feat: start project

---
+++ src/pages/index.tsx
@@ -0,0 +1,43 @@
+import { GetServerSideProps } from "next";
+import { Features } from "../components/Features";
+import { Hero } from "../components/Hero";
+import { Pricing } from "../components/Pricing";
+import { usePageQuery, PageDocument } from "../generated/graphql";
+import { client, ssrCache } from "../lib/urql";
+
+export default function Home() {
+  const [{ data }] = usePageQuery({
+    variables: {
+      slug: "home",
+    },
+  });
+
+  return (
+    <>
+      <Hero title={data.page.title} subtitle={data.page.subtitle} />
+      <Features />
+      <Pricing />
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  await client.query(PageDocument, { slug: "home" }).toPromise();
+
+  return {
+    props: {
+      urqlState: ssrCache.extractData(),
+    },
+  };
+};
+
+// export const getStaticProps: GetStaticProps = async () => {
+//   await client.query(PageDocument, { slug: "home" }).toPromise();
+
+//   return {
+//     props: {
+//       urqlState: ssrCache.extractData(),
+//     },
+//     revalidate: 10, //60 * 60 * 4, // 4 hours
+//   };
+// };
